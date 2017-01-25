faaaf94a983847bf1e627acf1942230ce8b27081
Fisayomi Ojuri <Fisayomi@Fisayomis-MacBook-Pro.local>
2017-01-25 18:08:43 +0100

    add express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static((__dirname)));
+app.use(express.static(('index.html')));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server listening on port 5000');

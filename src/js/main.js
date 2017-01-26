be4e8bfcb09b41c92f49e53d042c5c59570e56c1
Fisayomi Ojuri <Fisayomi@Fisayomis-MacBook-Pro.local>
2017-01-26 11:48:28 +0100

    Refactor main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,30 @@
 const app = angular.module('myApp', ['ngMessages']);
 
 app.controller('appCtrl', ($scope) => {
-  // $scope.createdIndex = {};
   $scope.name = [];
   $scope.invertedIndex = new Index();
-
   $scope.uploader = () => {
     if (window.File && window.FileReader && window.FileList) {
-      $scope.files = document.getElementById('upload').files[0];
-      if ($scope.files) {
-        $scope.name.push($scope.files.name);
-        const r = new FileReader();
-        r.onload = (e) => {
-          let file = e.target.result;
+      $scope.files = document.getElementById('upload').files;
+      //console.log($scope.files);
+      Object.keys($scope.files).forEach((file, index) => {
+        $scope.name.push($scope.files[index].name);
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          let fileContent = e.target.result;
           try {
-            file = JSON.parse(file);
-            $scope.invertedIndex.allBooks = $scope.invertedIndex.allBooks.concat(file);
+            fileContent = JSON.parse(fileContent);
+            $scope.invertedIndex.allBooks = $scope.invertedIndex.allBooks.concat(fileContent);
             $scope.invertedIndex.createIndex($scope.invertedIndex.allBooks);
-            $scope.invertedIndex.createIndex(file, $scope.files.name);
+            $scope.invertedIndex.createIndex(fileContent, $scope.files[index].name);
           } catch (err) {
             // console.log(err);
           }
         };
-        r.readAsText($scope.files);
-      } else {
-        alert('Kindly choose a file to upload');
-        }
+        reader.readAsText($scope.files[index]);
+      });
     } else {
-       // console.log('The File APIs are not fully supported by your browser.');
+      console.log('The File APIs are not fully supported by your browser.');
     }
   };
   $scope.create = () => {
@@ -35,6 +32,7 @@ app.controller('appCtrl', ($scope) => {
     const filen = $scope.e.options[$scope.e.selectedIndex].value;
     $scope.createdIndex = $scope.invertedIndex.getIndex(filen.replace(/\.json|\.|\s/g, ''));
     $scope.count = $scope.invertedIndex.docNum[filen.replace(/\.json|\.|\s/g, '')];
+    $scope.title = $scope.invertedIndex.documentTitle[filen.replace(/\.json|\.|\s/g, '')];
   };
   $scope.search = () => {
     $scope.e = document.getElementById('filename');
